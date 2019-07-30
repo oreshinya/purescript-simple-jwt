2375ebc8be7f65f3365a6d3535d933bf9fe08f02
Shinya Takahashi <s.takahashi313@gmail.com>
2019-07-30 23:09:54 +0900

    Add ReadForeign Jwt

--- src/Node/Simple/Jwt.purs
+++ src/Node/Simple/Jwt.purs
@@ -30,6 +30,7 @@ newtype Jwt = Jwt String
 
 derive newtype instance showJwt :: Show Jwt
 derive newtype instance eqJwt :: Eq Jwt
+derive newtype instance readForeingJwt :: ReadForeign Jwt
 derive newtype instance writeForeignJwt :: WriteForeign Jwt
 
 -- | Supported algorithms.
